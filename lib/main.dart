71ee6c7877b5a8283006a46437b785568928bca4
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-04-11 20:21:13 +0700

    feat: implement Center and Expanded

--- lib/main.dart
+++ lib/main.dart
@@ -19,18 +19,23 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
-        SizedBox(
-          width: 20,
-        ),
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        )
-      ],
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Image.asset('images/dice2.png'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
