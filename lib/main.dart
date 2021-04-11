b0c9614d4bd67fcadf6fa178ada642fd9a766418
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-04-11 20:10:48 +0700

    feat: implement Expanded with Image.asset

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,18 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Expanded(
+          child: Image.asset('images/dice1.png'),
+        ),
+        SizedBox(
+          width: 20,
+        ),
+        Expanded(
+          child: Image.asset('images/dice1.png'),
+        )
+      ],
+    );
   }
 }
